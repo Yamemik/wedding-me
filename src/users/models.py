a5049fc580dd189d13f3b748469c9e50f298d0d4
kkk <kuancarlos@mail.ru>
2024-12-16 18:31:33 +0500

    upd arch

---
+++ src/users/models.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, DateTime, String, Boolean
+from sqlalchemy.orm import relationship
+
+from ..common.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime, default=datetime.now)
+    email = Column(String, unique=True)
+    password = Column(String, )
+    surname = Column(String)
+    name = Column(String)
+    patr = Column(String)
+
+    is_admin = Column(Boolean, default=False)
+    
