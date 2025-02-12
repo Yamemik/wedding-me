3aacd9d6706b433c85cc6600254d4ff699fc539d
kkk <kuancarlos@mail.ru>
2025-02-12 21:57:13 +0500

    upd add entity gallaries

---
+++ src/gallaries/models.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, DateTime, String, Boolean
+from sqlalchemy.orm import relationship
+
+from ..common.database import Base
+
+
+class Gallery(Base):
+    __tablename__ = "gallaries"
+
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime, default=datetime.now)
+
+    
